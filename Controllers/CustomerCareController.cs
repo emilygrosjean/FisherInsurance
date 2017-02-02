4cab44dd51a3c6c10c14c921eb32d5f6799d00a2
Emily Grosjean <Emily Grosjean>
2017-02-01 19:09:53 -0500

    customize URL's

--- Controllers/CustomerCareController.cs
+++ Controllers/CustomerCareController.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 
 
-
+[Route("customer")]
 public class CustomerCareController : Controller
 {
     public IActionResult Index()
@@ -9,13 +9,22 @@ public class CustomerCareController : Controller
         return Ok("This is the index of the CustomerCareController");
     }
 
+    [Route("claims")]
     public IActionResult Quote()
     {
         return Ok("This is the claims page of the CustomerCareController");
     }
 
-    public IActionResult Claims()
+    [Route("fileclaim")]
+    public IActionResult NewClaim()
     {
-        return Quote();
+        return Ok("This is the fileclaim page of the CustomerCareController");
     }
+
+    [Route("myclaims")]
+    public IActionResult ClaimHistory()
+    {
+        return Ok("This is the claimstatus page of the CustomerCareController");
+    }
+
 }
